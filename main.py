94b384a6363d76c21f1056f9cc22b9a937fc75d4
Silvio Miranda <silviosmf@gmail.com>
2022-01-27 19:22:22 -0300

    a

---
+++ main.py
@@ -0,0 +1,6 @@
+import dash
+import dash_core_components as dcc
+import dash_core_components as html
+from dash.dependencies import Output, Input
+import dash_bootstrap-components as dbc
+print('teste')
